194f08b9665c54cb51e024c0cbfff9fe26c6ec46
Steven Wooding <steve@stevenwooding.com>
2019-08-16 12:23:27 +0100

    fix: Revert health response so tests are sucessful

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy | Repository change test.")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
